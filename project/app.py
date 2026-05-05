9b5e117ef193683962d07aa3269d38ad77a82aac
NAVAMY SHIBU <navamyshibukumar@gmail.com>
2026-05-05 21:47:12 +0530

    Update app.py

--- project/app.py
+++ project/app.py
@@ -1,8 +1,3 @@
-ameError: This app has encountered an error. The original error message is redacted to prevent data leaks. Full error details have been recorded in the logs (if you're on Streamlit Cloud, click on 'Manage app' in the lower right of your app).
-Traceback:
-File "/mount/src/crop-yield/project/app.py", line 306, in <module>
-    if shap_available:
-       ^^^^^^^^^^^^^^
 
 import streamlit as st
 import numpy as np
